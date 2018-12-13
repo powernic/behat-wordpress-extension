6bdb78fd23a71bc25ef7ad29039e27df2e514420
Paul Gibbs <djpaul@gmail.com>
2018-12-13 15:36:37 +0000

    Update mkdocs script for recent changes.

--- build/deploy-docs.sh
+++ build/deploy-docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
-pip install --user --upgrade pip
-pip install --user --upgrade pymdown-extensions pygments mkdocs mkdocs-material pymdown-extensions
-pip show mkdocs-material | grep -E ^Version
+pip3 install --user --upgrade pip
+pip3 install --user --upgrade pymdown-extensions pygments mkdocs mkdocs-material pymdown-extensions
+pip3 show mkdocs-material | grep -E ^Version
 mkdocs gh-deploy
 rm -fr ./site/
