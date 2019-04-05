1d1e00b244644d78b16748e4fcefee197aa3094d
Paul Gibbs <djpaul@gmail.com>
2019-04-06 00:37:34 +0100

    CI: first pass at new structure.

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Paths relative to root.
+
+# WordPress.
+vendor/bin/wp core install --path=$WP_WEBROOT --url=localhost:8000 \
+  --title=wordhat --skip-email \
+  --admin_email=wordpress@example.com \
+  --admin_user=wordpress --admin_password=password
