257b1cf828d95415199aea1f60f13ab28d5e3301
Paul Gibbs <djpaul@gmail.com>
2019-04-06 23:40:29 +0100

    CI: re-add old script to set sane WordPress defaults.
    This includes:
     - permalinks
     - disabling Gutenberg
     - making sure a theme is active.

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,5 +10,14 @@ vendor/bin/wp core install --path=$WP_WEBROOT --url=localhost:8000 \
   --admin_email=wordpress@example.com \
   --admin_user=admin --admin_password=password
 
-# Disable Gutenberg.
-vendor/bin/wp --path=$WP_WEBROOT --url=localhost:8000 plugin install disable-gutenberg --activate
+# Sane defaults.
+vendor/bin/wp theme activate --path=$WP_WEBROOT twentyseventeen
+vendor/bin/wp rewrite structure --path=$WP_WEBROOT '/%year%/%monthnum%/%postname%/'
+curl -o ${WP_WEBROOT}/wp-content/mu-plugins/disable-gutenberg.php https://gist.githubusercontent.com/paulgibbs/6d6309e0ea586d955e0b7b5573d5a642/raw/f8961ab10b818379c209359b36d9ad0d4ed9bbde/disable-gutenberg.php
+
+# The default widgets often repeat post titles and confuse Behat.
+for sidebar in $(vendor/bin/wp sidebar list --path=$WP_WEBROOT --format=ids); do
+  for widget in $(vendor/bin/wp widget list $sidebar --path=$WP_WEBROOT --format=ids); do
+    vendor/bin/wp widget delete --path=$WP_WEBROOT $widget
+  done;
+done;
