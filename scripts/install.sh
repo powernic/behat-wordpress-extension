433dc8644e6b47e3718d80d512a838b1c6421183
Paul Gibbs <djpaul@gmail.com>
2019-04-06 23:59:23 +0100

    CI: debugging

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,9 +17,8 @@ vendor/bin/wp plugin install disable-gutenberg --path=$WP_WEBROOT --activate
 
 # The default widgets often repeat post titles and confuse Behat.
 for sidebar in $(vendor/bin/wp sidebar list --path=$WP_WEBROOT --format=ids); do
-  echo $WP_WEBROOT
-  echo $sidebar
   for widget in $(vendor/bin/wp widget list $sidebar --path=$WP_WEBROOT --format=ids); do
+    echo $sidebar
     echo $widget
     vendor/bin/wp widget delete --path=$WP_WEBROOT $widget
   done;
