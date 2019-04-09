7e59e2892a8dc156510ab3fb637489ce28679867
Code Review Videos <chris@codereviewvideos.com>
2019-04-09 14:57:54 +0100

    Adds  'page_template' as a whitelisted CLI arg

--- src/Driver/Element/Wpcli/ContentElement.php
+++ src/Driver/Element/Wpcli/ContentElement.php
@@ -50,7 +50,7 @@ class ContentElement extends BaseElement
                 'ID', 'post_author', 'post_date', 'post_date_gmt', 'post_content', 'post_content_filtered', 'post_title',
                 'post_excerpt', 'post_status', 'post_type', 'comment_status', 'ping_status', 'post_password', 'post_name',
                 'to_ping', 'pinged', 'post_modified', 'post_modified_gmt', 'post_parent', 'menu_order', 'post_mime_type',
-                'guid', 'post_category',
+                'guid', 'post_category', 'page_template',
             ),
             $args
         );
